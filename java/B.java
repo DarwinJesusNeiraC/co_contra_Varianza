e400352eeac51f614cc8746f7406a33e803d2b06
dneirac <dneirac@unsa.edu.pe>
2022-11-27 16:14:29 -0500

    decendencia de interfaces

---
+++ java/B.java
@@ -0,0 +1,11 @@
+/*
+* @Autor: Darwin Neira Carrasco
+* @Email: dneirac@unsa.edu.pe
+* @Descripcion:
+*/
+
+public interface B extends A {
+  public void goo();
+
+}
+
---
+++ java/C.java
@@ -0,0 +1,10 @@
+/*
+* @Autor: Darwin Neira Carrasco
+* @Email: dneirac@unsa.edu.pe
+* @Descripcion:
+*/
+
+public interface C extends A {
+  public void roo(); 
+}
+
