90354680e44761248e19087fbd0c2f1ea47fb532
dneirac <dneirac@unsa.edu.pe>
2022-11-27 17:24:07 -0500

    probando retornando A en X, B en Y, ambos resiben Y

---
+++ java/X.java
@@ -0,0 +1,14 @@
+/*
+* @Autor: Darwin Neira Carrasco
+* @Email: dneirac@unsa.edu.pe
+* @Descripcion:
+*/
+
+public class X {
+  int x = 0;
+  public A foo(B b) {
+    System.out.format("X::foo(B), retorna A %d\n", x);
+    return b;
+  }
+}
+
---
+++ java/Y.java
@@ -0,0 +1,16 @@
+/*
+* @Autor: Darwin Neira Carrasco
+* @Email: dneirac@unsa.edu.pe
+* @Descripcion:
+*/
+
+public class Y extends X {
+  int y = 1;
+
+  @Override
+  public B foo(B a) {
+    System.out.format("Y::foo(B) retorna B %d\n", y);
+    return a;
+  }
+}
+
